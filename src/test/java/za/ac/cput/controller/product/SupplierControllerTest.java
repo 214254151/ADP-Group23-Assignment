ce307630b30dbb6c50fc10cd7cc62cae867d502a
RLX-Wilkinson <219071330@mycput.ac.za>
2021-10-20 19:40:54 +0200

    Controller Update

--- src/test/java/za/ac/cput/controller/product/SupplierControllerTest.java
+++ src/test/java/za/ac/cput/controller/product/SupplierControllerTest.java
@@ -26,8 +26,11 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 class SupplierControllerTest {
 
-    private static Supplier supplier = SupplierFactory.build("Checkers", "CheckersHQ@yahoo.com", "021 456 7891");
+    private static Supplier supplier = SupplierFactory.build("Checkers", "CheckersHQ@yahoo.com", "021456891");
     private static Supplier supplierV2 = SupplierFactory.build("Aneesa's Organics", "AOrganics@gmail.com", "021 455 56874");
+
+    private String securityUsername = "user";
+    private String securityPassword = "password";
 
     private final String baseURL = "http://localhost:8080/supplier";
 
@@ -39,57 +42,55 @@ class SupplierControllerTest {
     @Test
     void create() {
         String url = baseURL + "/create";
-        ResponseEntity<Supplier> postResponse = rTemplate.postForEntity(url, supplier, Supplier.class);
-
-        assertNotNull(postResponse);
-        assertNotNull(postResponse.getBody());
-        supplier = postResponse.getBody();
-        System.out.println("Supplier Saved: " + supplier);
+        HttpHeaders header = new HttpHeaders();
+        header.setBasicAuth(securityUsername, securityPassword);
+        ResponseEntity<Supplier> postResponse;
+        HttpEntity<Supplier> request = new HttpEntity<>(supplier, header);
+        postResponse = rTemplate.exchange(url, HttpMethod.POST, request, Supplier.class);
         assertEquals(supplier.getSupplierID(), postResponse.getBody().getSupplierID());
+        System.out.println(postResponse.getBody());
     }
 
     @Order(2)
     @Test
-    void createSecond() {
-        String url = baseURL + "/create";
-        ResponseEntity<Supplier> postResponse = rTemplate.postForEntity(url, supplierV2, Supplier.class);
+    void read() {
+        String url = baseURL + "/read/" + supplier.getSupplierID();
+        HttpHeaders header = new HttpHeaders();
+        header.setBasicAuth(securityUsername, securityPassword);
+        System.out.println("URL: " + url);
+        HttpEntity<Supplier> request = new HttpEntity<>(null, header);
+        ResponseEntity<Supplier> postResponse = rTemplate.exchange(url, HttpMethod.GET, request, Supplier.class);
 
-        assertNotNull(postResponse);
-        assertNotNull(postResponse.getBody());
-        supplierV2 = postResponse.getBody();
-        System.out.println("Supplier Saved: " + supplierV2);
-        assertEquals(supplierV2.getSupplierID(), postResponse.getBody().getSupplierID());
+        assertEquals(supplier.getSupplierID(), postResponse.getBody().getSupplierID());
+        System.out.println(postResponse);
     }
 
     @Order(3)
     @Test
-    void read() {
-        String url = baseURL + "/read/" + supplier.getSupplierID();
-        System.out.println("URL: " + url);
-        ResponseEntity<Supplier> response = rTemplate.getForEntity(url, Supplier.class);
-        assertEquals(supplierV2.getSupplierID(), response.getBody().getSupplierID());
+    void update() {
+        Supplier supplierChange = new Supplier.Builder().copy(supplier).setSupplierEmail("IchangedEmail.com").build();
+        String url = baseURL + "/update";
+        HttpHeaders header = new HttpHeaders();
+        header.setBasicAuth(securityUsername, securityPassword);
+
+        System.out.println("Updated Supplier: " + supplierChange);
+        HttpEntity<Supplier> request = new HttpEntity<>(supplierChange, header);
+        ResponseEntity<Supplier> postResponse = rTemplate.exchange(url, HttpMethod.POST, request, Supplier.class);
+        assertNotNull(postResponse.getBody());
+        System.out.println(postResponse);
     }
 
     @Order(4)
     @Test
-    void update() {
-        Supplier update = new Supplier.Builder().copy(supplier).setSupplierEmail("Checkers@gmail.com").build();
-        String url = baseURL + "/update";
-        System.out.println("Updated Supplier: " + update);
-        ResponseEntity<Supplier> response = rTemplate.postForEntity(url, update, Supplier.class);
-        assertNotNull(response.getBody());
-    }
-
-    @Order(5)
-    @Test
     void delete() {
         String url = baseURL + "/getAll";
         HttpHeaders header = new HttpHeaders();
+        header.setBasicAuth(securityUsername, securityPassword);
         HttpEntity<String> httpE = new HttpEntity<>(null, header);
-        ResponseEntity<String> responseGetAll = rTemplate.exchange(url, HttpMethod.GET, httpE, String.class);
-        System.out.println("Get All: ");
-        System.out.println(responseGetAll);
-        System.out.println(responseGetAll.getBody());
+        ResponseEntity<String> postResponseGetAll = rTemplate.exchange(url, HttpMethod.GET, httpE, String.class);
+        System.out.println("Get All: (Suppliers)");
+        System.out.println(postResponseGetAll);
+        System.out.println(postResponseGetAll.getBody());
     }
 
 }
